9735c0facc17bc032623e06bf5351ee9260342e7
Marcel Reis <marcelclemente.msc@gmail.com>
2020-06-10 22:07:20 +0200

    Class structure and wavguide functionality

--- fdbpm.py
+++ fdbpm.py
@@ -1,81 +1,122 @@
-#%%
 import numpy as np
 import time as t
 import matplotlib.pyplot as plt
 
-start=t.time()
+class FdBpm:
+    def __init__(self):
+        # window properties
+        self.NUM_SAMPLES = 50
+        self.LENGTH = 1000
 
-x1 = -60e-6
-x2 = 60e-6
-num_samples = 50
-dx = (x2-x1)/num_samples
-dy = 1e-6
-x = np.linspace(x1, x2, num_samples)
-W0 = 8e-6
-n_index = np.ones((num_samples,))
-nmax = 1
-nmin = 1
-n_bar = (nmax+nmin)/2
-l_ambda = 0.8e-6
-k0 = 2*np.pi/l_ambda
+        # Physical Properties
+        self.L=120E-6
+        self.dx = self.L/self.NUM_SAMPLES
+        self.dy = 1e-6
+        self.x = np.linspace(-self.L/2, +self.L/2, self.NUM_SAMPLES)
+        self.l_ambda = 0.8e-6
+        
+    def create_source(self,plotOn=True):
+        
 
-#%% Gauss
-modo = np.array(np.exp(-(x/W0)**2))
-modo2=modo
+        waist = 8e-6
+        self.light=self.gauss(self.x,waist)
 
-k = k0*n_index
-k_bar = k*n_bar
+        if plotOn:
+            plt.plot(self.x,self.light)
+            plt.show()
+        
 
-length=1000
+    def gauss(self,x,w0):
+        return np.array(np.exp(-(x/w0)**2))
 
-h = dy
-ro = dy/(dx**2)
-A = 1j/(2*k_bar)
-B = 1j*(k**2-k_bar**2)/(2*k_bar)
-a = -ro*A
-b = 2*(1+ro*A)-h*B
-c = a
+    def create_guides(self,plotOn=True):
 
-matrix = np.zeros((num_samples,num_samples),dtype='complex_')
+        guides = np.ones((self.NUM_SAMPLES,))
+        guides[20:30]+=0.4
+        nmax = np.max(guides)
+        nmin = np.min(guides)
+        avg_guide = (nmax+nmin)/2
 
-#%% Tridiagonal Matrix
-index=np.arange(num_samples)
+        (self.guides,self.avg_guide)=(guides,avg_guide)
 
-index0=index[:-1]
-matrix[index0,index0+1]=a[index0]
+        if plotOn:
+            plt.plot(self.x,guides)
+            plt.show()
 
-index1=index[1:]
-matrix[index1,index1-1]=c[index1]
+        return (guides,avg_guide)
 
-matrix[index,index]=b[index]
+    def make_tri_matrix(self):
 
-# print((matrix_original==matrix).all())
-#%% Propagation
-modo = np.array(np.exp(-(x/W0)**2))
-zz = np.zeros((length,num_samples))
-d = np.zeros((num_samples),dtype='complex_')
+        k0 = 2*np.pi/self.l_ambda
+        k = k0*self.guides
+        k_bar = k*self.avg_guide
 
-index=np.arange(1,num_samples-1)
-for n in range(length):         # cannot be transferred to numpy cause of np.linalg.solve in between lines
-    d[index]=(2*(1-ro*A[index])+h*B[index])*modo[index]+ro*A[index]*(modo[index-1]+modo[index+1])
-    d[0] = (2*(1-ro*A[0])+h*B[0])*modo[0]+ro*A[0]*(modo[1])
-    d[-1] = (2*(1-ro*A[-1])+h*B[-1])*modo[-1]+ro*A[-1]*(modo[-2])
+        self.h = self.dy
+        self.ro = self.dy/(self.dx**2)
+        self.A = 1j/(2*k_bar)
+        self.B = 1j*(k**2-k_bar**2)/(2*k_bar)
+        a = -self.ro*self.A
+        b = 2*(1+self.ro*self.A)-self.h*self.B
 
-    # modo = np.linalg.lstsq(matrix,d)[0]
-    modo = np.linalg.solve(matrix,d) #fastest option
-    zz[n,:] = np.abs(modo)
+        tridiag_matrix = np.zeros((self.NUM_SAMPLES,self.NUM_SAMPLES),dtype='complex_')
 
-#%%
-# print(modo[0])
+        index=np.arange(self.NUM_SAMPLES)
 
-end = t.time()
-print("Time elapsed:%0.4f seconds"%(end-start))
+        index0=index[:-1]
+        tridiag_matrix[index0,index0+1]=a[index0]
 
-#%%
+        index1=index[1:]
+        tridiag_matrix[index1,index1-1]=a[index1]
 
-zz=zz[::np.int(np.floor(length/num_samples)),:]
+        tridiag_matrix[index,index]=b[index]
 
-plt.imshow(zz,cmap="jet",interpolation='bilinear')
-plt.show()
+        self.tridiag_matrix=tridiag_matrix
 
-# %%
+    def calculate_propagation(self, plotOn=True):
+
+        if not hasattr(self,'tridiag_matrix'):
+            self.make_tri_matrix()
+
+        start=t.time()
+
+        (A,B,ro,light,tridiag_matrix,h)=(self.A,self.B,self.ro,self.light,self.tridiag_matrix,self.h)
+
+        propag = np.zeros((self.LENGTH,self.NUM_SAMPLES))
+        d = np.zeros((self.NUM_SAMPLES),dtype='complex_')
+
+        index=np.arange(1,self.NUM_SAMPLES-1)
+
+        for n in range(self.LENGTH):         # cannot be transferred to numpy cause of np.linalg.solve in between lines
+            d[index]=(2*(1-ro*A[index])+h*B[index])*light[index]+ro*A[index]*(light[index-1]+light[index+1])
+            d[0] = (2*(1-ro*A[0])+h*B[0])*light[0]+ro*A[0]*(light[1])
+            d[-1] = (2*(1-ro*A[-1])+h*B[-1])*light[-1]+ro*A[-1]*(light[-2])
+
+            # self.modo = np.linalg.lstsq(tridiag_matrix,d)[0]
+            light = np.linalg.solve(tridiag_matrix,d) #fastest option
+            propag[n,:] = np.abs(light)
+
+        end = t.time()
+        print("Time elapsed:%0.4f seconds"%(end-start))
+
+        self.propag=propag
+
+        if plotOn:
+            propag_img=propag[::np.int(np.floor(self.LENGTH/self.NUM_SAMPLES)),:]
+            
+            fig,ax=plt.subplots()
+            ax.imshow(propag_img,cmap="plasma",interpolation='bilinear',extent=[-self.L/2*1E6,+self.L/2*1E6,self.LENGTH,0],aspect='auto')
+            ax.set_xlabel(r"x ($\mu$m)")
+            ax.set_ylabel(r"Length ($\mu$m)")
+
+            plt.show()
+
+        
+
+    
+
+if __name__ == "__main__":
+    fd=FdBpm()
+    plotOn=False
+    fd.create_source(plotOn=plotOn)
+    fd.create_guides(plotOn=plotOn)
+    fd.calculate_propagation()
